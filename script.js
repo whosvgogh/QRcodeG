e475f17159360443e20f658365d39b8d334129fe
whosvgogh <133163414+whosvgogh@users.noreply.github.com>
2023-08-18 17:45:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const wrapper = document.querySelector(".wrapper"),
+qrInput = wrapper.querySelector(".form input"),
+generateBtn = wrapper.querySelector(".form button"),
+qrImg = wrapper.querySelector(".qr-code img");
+let preValue;
+
+generateBtn.addEventListener("click", () => {
+    
+
+}
