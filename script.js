596c98fba6d196defaed5fd8c48d38c22a76f83b
whosvgogh <133163414+whosvgogh@users.noreply.github.com>
2023-08-25 17:23:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const wrapper = document.querySelector(".wrapper"),
-qrInput = wrapper.querySelector(".form input"),
-generateBtn = wrapper.querySelector(".form button"),
-qrImg = wrapper.querySelector(".qr-code img");
+    qrInput = wrapper.querySelector(".form input"),
+    generateBtn = wrapper.querySelector(".form button"),
+    qrImg = wrapper.querySelector(".qr-code img");
 let preValue;
 
 generateBtn.addEventListener("click", () => {
-    
+    let qrValue = qrInput.value.trim();
+    if(!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innertext = "Generate QR code";
+    });
+});
 
-}
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value.trim()) {
+        wrapper.classList.remove("active");
+        preValue = "";
+
+    }
+});
